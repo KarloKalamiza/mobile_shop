a0a07ad8e5732aa2586ccc5e4d6652690d5e7463
Karlo Kalamiza <80110697+KarloKalamiza@users.noreply.github.com>
2024-01-29 12:18:20 +0100

    Firebase donfigured and set up I used flutterfire configure CLI command and keytool -list -v -keystore "C:\Users\karlo\.android\debug.keystore" -alias androiddebugkey -storepass android -keypass android for generating android sha1

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
+import 'package:get/get.dart';
 import 'app.dart';
+import 'data/repositories/authentication_repo.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
   // TODO: Add widgets Binding
   // TODO: Init Local Storage
   // TODO: Await Native Splash
   // TODO: Initialize Firebase
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  ).then((value) => Get.put(AuthenticationRepository()));
   // TODO: Initialize Authentication
 
   runApp(const App());
